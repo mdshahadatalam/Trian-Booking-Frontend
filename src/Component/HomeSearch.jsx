5f49c4732b88ea6ff1869368298e4578d66d65b6
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-01-08 19:39:07 -0800

    added

--- src/Component/HomeSearch.jsx
+++ src/Component/HomeSearch.jsx
@@ -8,8 +8,21 @@ export const HomeSearch = () => {
        <BookinSearch/>
     <div className="container">
         <div className="row">
+              <ul className="nav nav-tabs mt-5 d-flex justify-between align-items-center" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <p className="nav-link navLi active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home"         role="tab" aria-controls="home" aria-selected="true">Popular Tour Packages</p>
+        </li>
+        <li className="nav-item" role="presentation">
+          <p className="nav-link navLiAll" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button"         role="tab" aria-controls="profile" aria-selected="false">/ view all tours</p>
+        </li>
+      </ul>
 
-        </div>
+
+         <div className="tab-content" id="myTabContent">
+           <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Home</div>
+           <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Profile</div>
+         </div>
+    </div>
     </div>
    </section>
    </>
