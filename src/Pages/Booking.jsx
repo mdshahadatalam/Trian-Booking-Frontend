8d1df1aa8448839f10e0d1ad7560383462098cce
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-01-09 18:58:01 -0800

    smal

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -10,7 +10,7 @@ export const Booking = () => {
         <section className='bookingSec'>
              <NavMain/>
              <BookinSearch/>
-            <div className="container">
+            <div className="container BookingRes">
                 <div className="row py-5">
                     
                     <div className="col-lg-2"></div>
@@ -131,6 +131,43 @@ export const Booking = () => {
                     
                 </div>
             </div>
+
+                  {/* small device  */}
+
+                    <div className="container py-5  smallBooking d-lg-none">
+                         <div className="row">
+                              <div className="col-md-4">
+                                  <div className='bg-white rounded-md shadow-md p-2 bookingOper mt-4    transition duration-300 ease-in-out transform  hover:shadow-lg hover:scale-105 '>
+                                       <div className='d-flex justify-center py-3'><span className='text-3xl '><FaTrainSubway /></span></div>
+                                        <div className='text-center'><span className=' text-slate-600 font-serif'>12:00 - 12:00</span> </div>
+                                           <div className='text-center'><span className=' text-slate-600 font-serif py-2 font-bold tracking-wide'>Sai Gon - Ha Noi</span> </div>  
+                                        
+
+                                        <p className='text-slate-700 font-mono text-sm py-2 text-center'>first class - 24hrs00mins travel</p>
+                                  </div>
+                              </div>
+                              <div className="col-md-4">
+                              <div className='bg-white rounded-md shadow-md p-2 bookingOper mt-4    transition duration-300 ease-in-out transform  hover:shadow-lg hover:scale-105 '>
+                                       <div className='d-flex justify-center py-3'><span className='text-3xl '><FaTrainSubway /></span></div>
+                                        <div className='text-center'><span className=' text-slate-600 font-serif'>12:00 - 12:00</span> </div>
+                                           <div className='text-center'><span className=' text-slate-600 font-serif py-2 font-bold tracking-wide'>Sai Gon - Ha Noi</span> </div>  
+                                        
+
+                                        <p className='text-slate-700 font-mono text-sm py-2 text-center'>first class - 24hrs00mins travel</p>
+                                  </div>
+                              </div>
+                              <div className="col-md-4">
+                              <div className='bg-white rounded-md shadow-md p-2 bookingOper  mt-4   transition duration-300 ease-in-out transform  hover:shadow-lg hover:scale-105 '>
+                                       <div className='d-flex justify-center py-3'><span className='text-3xl '><FaTrainSubway /></span></div>
+                                        <div className='text-center'><span className=' text-slate-600 font-serif'>12:00 - 12:00</span> </div>
+                                           <div className='text-center'><span className=' text-slate-600 font-serif py-2 font-bold tracking-wide'>Sai Gon - Ha Noi</span> </div>  
+                                        
+
+                                        <p className='text-slate-700 font-mono text-sm py-2 text-center'>first class - 24hrs00mins travel</p>
+                                  </div>
+                              </div>
+                         </div>
+                    </div>
         </section>
      </>
   )
